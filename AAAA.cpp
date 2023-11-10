784ca954fc9d7910d3e40b6ef9715f4d2a2a0e1a
Owen Chen <72772851+brokentelescope@users.noreply.github.com>
2023-11-10 15:12:06 -0500

    Update main.cpp

--- AAAA.cpp
+++ AAAA.cpp
@@ -1,12 +1,97 @@
-#include <bits/stdc++.h>
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
 
-using namespace std;
 
-int n, m, k;
+#include "vex.h"
 
+using namespace vex;
+
+// Brain should be defined by default
+brain Brain;
+
+// Robot configuration code.
+
+motor armMotor = motor(PORT10, ratio18_1, false);
+motor LeftDriveSmart = motor(PORT4, ratio6_1, false);
+motor RightDriveSmart = motor(PORT5, ratio6_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 317.5, 117.475, mm, 1);
+
+controller Controller1 = controller(primary);
+
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+#pragma endregion VEXcode Generated Robot Configuration
+
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       {Merivale High School}                                    */
+/*    Created:      {11/23/2022}                                              */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// Include the V5 Library
+#include "vex.h"
+  
+// Allows for easier use of the VEX Library
+using namespace vex;
+competition Competition;
+
+void runOnAutonomous(void) {
+  
+}
+
+void spin(void) {
+  armMotor.spin(forward);
+}
+
+void stop_motor(void) {
+  armMotor.stop();
+}
+
+void runOnDriverControl(void) {
+  Brain.Screen.print("Running teleop");
+
+  armMotor.setVelocity(50, percent);
+
+  
+  while (true) {
+    // arm 
+    Controller1.ButtonL1.pressed(spin);
+    Controller1.ButtonL1.released(stop_motor);
+
+    // drivetrain
+    if (Controller1.Axis3.position() > 0.0) {
+      LeftDriveSmart.setVelocity((Controller1.Axis3.position() + Controller1.Axis4.position())/1.5, percent);
+      RightDriveSmart.setVelocity((Controller1.Axis3.position() - Controller1.Axis4.position())/1.5, percent);
+    }
+
+    else {
+      RightDriveSmart.setVelocity((Controller1.Axis3.position() + Controller1.Axis4.position())/1.5, percent);
+      LeftDriveSmart.setVelocity((Controller1.Axis3.position() - Controller1.Axis4.position())/1.5, percent);
+    }
+    
+    LeftDriveSmart.spin(forward);
+    RightDriveSmart.spin(forward); 
+  }
+
+}
 
 int main() {
-    cin.sync_with_stdio(0); cin.tie(0);
+  
+  runOnDriverControl();
+  // Competition.autonomous(runOnAutonomous);
+  Competition.drivercontrol(runOnDriverControl);
 
+  while (true) {
+    wait(0.05, seconds);
+  }
 
 }
