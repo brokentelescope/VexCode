2f87227d34a67ec7f5517cf5313bfcb9173125b8
Owen Chen <72772851+brokentelescope@users.noreply.github.com>
2023-11-17 15:58:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,14 +14,40 @@ using namespace vex;
 // Brain should be defined by default
 brain Brain;
 
+
+// START V5 MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+
 // Robot configuration code.
 
+//configure motors
 motor armMotor = motor(PORT10, ratio18_1, false);
-motor LeftDriveSmart = motor(PORT4, ratio6_1, false);
-motor RightDriveSmart = motor(PORT5, ratio6_1, true);
+motor armMotor2 = motor (PORT9, ratio18_1, false);
+motor LeftDriveSmart = motor(PORT7, ratio6_1, false);
+motor RightDriveSmart = motor(PORT8, ratio6_1, true);
 drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 317.5, 117.475, mm, 1);
 
+//configure controllers
 controller Controller1 = controller(primary);
+
+
+
+// Helper to make playing sounds from the V5 in VEXcode easier and
+// keeps the code cleaner by making it clear what is happening.
+void playVexcodeSound(const char *soundName) {
+  printf("VEXPlaySound:%s\n", soundName);
+  wait(5, msec);
+}
+
+
 
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
@@ -48,25 +74,44 @@ void runOnAutonomous(void) {
   
 }
 
+//spin function
 void spin(void) {
-  armMotor.spin(forward);
+    //motor on port 10 is forward and motor in port 9 is in reverse on nov 17
+    armMotor.spin(forward);
+    armMotor2.spin(reverse);
+  }
+
+void spinrev(void) {
+  //motor on port 9 is forward and motor in port 10 is in reverse on nov 17
+    armMotor.spin(reverse);
+    armMotor2.spin(forward);
 }
 
 void stop_motor(void) {
   armMotor.stop();
+  armMotor2.stop();
 }
 
 void runOnDriverControl(void) {
+  Brain.Screen.setFont(mono20);
   Brain.Screen.print("Running teleop");
 
   armMotor.setVelocity(50, percent);
 
-  
+
   while (true) {
-    // arm 
+    
     Controller1.ButtonL1.pressed(spin);
     Controller1.ButtonL1.released(stop_motor);
+    Controller1.ButtonR1.pressed(spinrev);
+    Controller1.ButtonR1.released(stop_motor);
+    Brain.Screen.setCursor(500,-100);
+    Brain.Screen.clearScreen();
+    Brain.Screen.setPenColor(blue);
+    Brain.Screen.setFillColor(red);
+    Brain.Screen.drawRectangle(0, 0, 100, 100);
 
+    
     // drivetrain
     if (Controller1.Axis3.position() > 0.0) {
       LeftDriveSmart.setVelocity((Controller1.Axis3.position() + Controller1.Axis4.position())/1.5, percent);
@@ -80,6 +125,7 @@ void runOnDriverControl(void) {
     
     LeftDriveSmart.spin(forward);
     RightDriveSmart.spin(forward); 
+    
   }
 
 }
